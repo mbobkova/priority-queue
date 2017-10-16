e8a407181ef2b31bee2a0ce39b7e9c475a5922ab
mbobkova <masha.bobkova997@gmail.com>
2017-10-16 23:19:16 +0300

    priority-queue: Completed Queue class

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,30 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		this.heap.push(data, priority);
+		if(this.heap.heapSize > this.maxSize) throw new Error ('Heap is full')
 	}
 
 	shift() {
-
+		if(!this.heap.isEmpty()) {
+			var nodeValue = this.heap.pop();
+			return nodeValue;
+		} else throw new Error ('This queue is empty');
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		if (this.heap.isEmpty()) {
+			return true;
+		} else return false;
 	}
 }
 
